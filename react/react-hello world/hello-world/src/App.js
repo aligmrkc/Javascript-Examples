3e8fcf88d3537dfb73fe1ada1a1525e5e15d8ad5
Ali GÜMRÜKÇÜ <ali.gumrukcu@etiya.com>
2020-09-01 18:19:35 +0300

    react hello and others lessons

---
+++ react/react-hello world/hello-world/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+
+
+function App() {
+  return (
+    <div className="App">
+     <Greet/>
+     <Welcome/> 
+     <Hello/>
+     
+     
+    </div>
+  );
+}
+
+export default App;
