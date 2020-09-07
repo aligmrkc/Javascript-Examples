3671c597fe596a3eb1974fa5ca43d2a5cf784f3c
Ali GÜMRÜKÇÜ <ali.gumrukcu@etiya.com>
2020-09-07 11:21:07 +0300

    css okey

--- react/react-todo-list/my-app/src/App.js
+++ react/react-todo-list/my-app/src/App.js
@@ -5,9 +5,14 @@ import './App.css';
 class App extends React.Component{
   render(){
     return(
-     <header>
-       
-     </header>
+      <div className="App">
+          <header>
+              <form id="to-do-form">
+                <input type="text" placeholder="Enter Text"></input>
+                <button type="submit">Add</button>
+              </form>
+          </header>
+      </div>
     )
   }
 }
