6d42929af2f40bf2f22f06183d9a1c393978d6a8
Ali GÜMRÜKÇÜ <ali.gumrukcu@etiya.com>
2020-09-17 17:25:49 +0300

    redux-counter ok

---
+++ react/Redux/redux-konu/src/reducers/index.js
@@ -0,0 +1,17 @@
+import counterReducer from './counter';
+import loggedReducer from './loggedReducer';
+import {combineReducers} from 'redux';
+
+
+const allReducers=combineReducers({
+    counter:counterReducer,
+    isLogged:loggedReducer
+});
+
+
+export default allReducers;
+
+
+
+
+
