e4bd85397ee8c2d942d891feb5feb6e856d5d429
Ali GÜMRÜKÇÜ <ali.gumrukcu@etiya.com>
2020-10-26 15:09:05 +0300

    blog son

--- reactlessons/react-blog/redux-blog/src/components/Home.js
+++ reactlessons/react-blog/redux-blog/src/components/Home.js
@@ -2,26 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import user from '../user-2.png'
+import {connect} from 'react-redux';
 
 
 class Home extends Component{
-    state={
-        posts:[]
-    }
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(
-            res=>{
-               
-                this.setState({
-                    posts:res.data.slice(0,10)
-                    
-                })
-                console.log(res)
-            })
-    }
+    
     render(){
-        const {posts}=this.state
+        const {posts}=this.props
         const postList=posts.length ?(   //Ternary içinde
             posts.map(post=>{
                 return(
@@ -56,4 +43,10 @@ class Home extends Component{
     
 }
 
-export default Home;
+const mapStateToProps=(state)=>{
+    return{
+        posts:state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home);
--- reactlessons/react-blog/redux-blog/src/reducers/rootReducer.js
+++ reactlessons/react-blog/redux-blog/src/reducers/rootReducer.js
@@ -1,10 +1,11 @@
-const initState={
+const initState=
+{
     posts:[
-        {id: 1,title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
+        {id: '1',title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
        
-        ,{id: 2,title: 'ali aut facere repellat provident occaecati excepturi optio reprehenderit',body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
+        ,{id: '2',title: 'ali aut facere repellat provident occaecati excepturi optio reprehenderit',body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
         
-        ,{id: 3,title: 'ahmet aut facere repellat provident occaecati excepturi optio reprehenderit2', body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
+        ,{id: '3',title: 'ahmet aut facere repellat provident occaecati excepturi optio reprehenderit2', body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
 
     
     
