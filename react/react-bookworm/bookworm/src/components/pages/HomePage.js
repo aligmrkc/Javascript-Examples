91766c3a25a626f686f85e4f98e723798ba1ac02
aligumrukcu <ali.gumrukcu@etiya.com>
2020-12-15 14:22:01 +0300

    react denem

---
+++ react/react-bookworm/bookworm/src/components/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link,Route} from 'react-router-dom'
+
+const HomePage=()=>(
+    <div>
+        <h1>Home Page</h1>
+        <Link to="/login">Login</Link>
+        <link></link>
+      
+    </div>
+)
+
+export default HomePage;
